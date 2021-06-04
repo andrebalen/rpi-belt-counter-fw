877dbc817e380a800caf36914cacbac1821a1483
johannes aernoudts <johannes_aernoudts@hotmail.com>
2021-06-04 20:55:23 -0300

    Delete cam test2.py

--- cam test2.py
+++
@@ -1,8 +0,0 @@
-from picamera import PiCamera
-from time import sleep
-
-camera = PiCamera()
-camera.start_preview(alpha=192)
-sleep(1)
-camera.capture("/hone/pi/desktop/pic.jpg")
-camera.stop_preview()  
